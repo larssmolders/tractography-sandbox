440fa6c37f00458a42e23848e23139217a6501c2
larssmolders <l.smolders@student.tue.nl>
2020-04-02 11:55:18 +0200

    Added adjustable stopping criterion for SimpleStreamlineSolver

--- Solvers/simplestreamlinesolver.py
+++ Solvers/simplestreamlinesolver.py
@@ -2,13 +2,15 @@ from Solvers.initialvaluesolver import InitialValueSolver
 import numpy as np
 
 class SimpleStreamlineSolver(InitialValueSolver):
-    def __init__(self, step_size, interpolator=None):
+    def __init__(self, step_size, interpolator=None, stopping_norm=1e-5):
         super().__init__(interpolator)
         self.step_size = step_size
+        self.stopping_norm = stopping_norm
 
     def solve(self, x0):
         cur_pos = x0
         result = []
+        dim = len(x0)
         while True:
             # observe derivative
             derivative = self.get_dataterm_at(cur_pos)
@@ -20,7 +22,7 @@ class SimpleStreamlineSolver(InitialValueSolver):
             result.append(np.array(cur_pos))
 
             # check if we should stop
-            if np.linalg.norm(derivative) < 1e-4:
+            if np.linalg.norm(derivative) < self.stopping_norm:
                 break
 
-        return result
+        return np.reshape(result, (-1, dim))
